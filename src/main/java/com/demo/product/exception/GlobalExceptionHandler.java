96008a6e79609689a9b092b6e30e029d5fa6e8c8
Duc Hieu <hieubui419@gmail.com>
2024-08-01 12:27:49 +0700

    add exception

---
+++ src/main/java/com/demo/product/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.demo.product.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception e) {
+		e.printStackTrace();
+		return ResponseEntity.status(500).body("Error");
+	}
+}
